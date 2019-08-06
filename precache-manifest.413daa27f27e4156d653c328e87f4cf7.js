e664125faa37cbcaa799d7b922b7ad603967809e
Turbocake64 <micahrabinowitz@gmail.com>
2019-08-06 11:38:45 -0700

    Updates

--- precache-manifest.413daa27f27e4156d653c328e87f4cf7.js
+++ precache-manifest.413daa27f27e4156d653c328e87f4cf7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "81368702826b5ecc615a",
+    "revision": "0d226158f18e2e1bdbc2",
     "url": "/Tannin/static/css/main.3a2c0abc.chunk.css"
   },
   {
-    "revision": "81368702826b5ecc615a",
-    "url": "/Tannin/static/js/main.e48d1048.chunk.js"
+    "revision": "0d226158f18e2e1bdbc2",
+    "url": "/Tannin/static/js/main.746c48bd.chunk.js"
   },
   {
     "revision": "dafb2ad3982900ac4088",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/Tannin/static/js/2.87f52fb5.chunk.js"
   },
   {
-    "revision": "59e8742bdd9f72c004a7733a267a3a5b",
+    "revision": "8cd6cffa6e4595e9de6386f63887ccdb",
     "url": "/Tannin/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Tannin/precache-manifest.b4e93413c4d9aba6f16a3b78ebcd3cc4.js"
+  "/Tannin/precache-manifest.413daa27f27e4156d653c328e87f4cf7.js"
 );
 
 workbox.clientsClaim();
